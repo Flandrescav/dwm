fd670df429335dbc659b658d41c3ede7aae92e22
flandresca <flandresca@163.com>
2022-12-04 22:11:27 +0800

    修改了全屏的bug

--- dwm.c
+++ dwm.c
@@ -1795,11 +1795,11 @@ Layout *last_layout;
 void
 fullscreen(const Arg *arg)
 {
-	if (selmon->showbar) {
-		for(last_layout = (Layout *)layouts; last_layout != selmon->lt[selmon->sellt]; last_layout++);
-		setlayout(&((Arg) { .v = &layouts[2] }));
-	} else {
-		setlayout(&((Arg) { .v = last_layout }));
+	if (selmon->sel){
+		if (selmon->showbar)
+			setfullscreen(selmon->sel, 1);
+		else
+			setfullscreen(selmon->sel, 0);
 	}
 	togglebar(arg);
 }
